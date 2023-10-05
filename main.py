1d35fecb46ac108411ea0a0bc23a504be411c660
Soumil Datta <soumildatta@gmail.com>
2023-10-04 20:13:48 -0500

    Make main function logic more modular

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import markdown 
 import pdfkit 
+from typing import Optional
 import argparse
 import os 
 import re
@@ -44,49 +45,39 @@ def process_folder_into_folder(input_folder, output_folder, css_file):
 def process_folder_into_file(input_folder, output_file, css_file):
     convert_combined(input_folder, output_file, css_file)
 
+def determine_conversion_type(input_path: str, output_path: str) -> Optional[str]:
+    input_ext = input_path.split('.')[-1] if '.' in input_path else None
+    output_ext = output_path.split('.')[-1] if '.' in output_path else None
+    
+    if input_ext == 'md' and output_ext == 'pdf':
+        return 'single'
+    elif input_ext != 'md' and output_ext == 'pdf':
+        return 'combined'
+    elif input_ext != 'md' and output_ext != 'pdf':
+        return 'folder'
+    else:
+        return None
+
 if __name__ == "__main__":
-    # parser = argparse.ArgumentParser(description='Convert Markdown to PDF.')
-    # parser.add_argument('--type', type=str, choices=['single', 'folder' 'combined'], required=True, help='Specify whether to process a single file or multiple files in a folder.')
-    # parser.add_argument('--input', type=str, required=True, help='Input Markdown file or folder.')
-    # parser.add_argument('--output', type=str, required=True, help='Output PDF file.')
-    # parser.add_argument('--css', type=str, required=False, help='Optional CSS file for styling.')  # Optional
-
-    # args = parser.parse_args()
-
-    # type = args.type
-    # input_file = args.input
-    # output_file = args.output
-    # css_file = args.css
-
     input_item = input('Input markdown file or folder: ')
     output_item = input('Input name of output pdf, or a folder to output pdfs to: ')
     css_item = input('CSS File: ')
-
-    # Check if input file exists
+    
     if not os.path.exists(input_item):
-        print(f"Error: Input file '{input_item}' not found.")
+        print(f"Error: Input item '{input_item}' not found.")
         exit(1)
-
-    # Perform conversion
-    split_input = input_item.split('.')
-    split_output= output_item.split('.')
-
-    type = ''
-    if len(split_input) > 1:
-        if split_input[-1] == 'md':
-            if split_output[-1] == 'pdf':
-                type = 'single'
-        else:
-            if split_output[-1] == 'pdf':
-                type = 'combined'
-            else:
-                type = 'folder'
-
-    if type == 'single':
+        
+    conversion_type = determine_conversion_type(input_item, output_item)
+    
+    if conversion_type is None:
+        print("Invalid input and output types.")
+        exit(1)
+        
+    if conversion_type == 'single':
         convert(input_item, output_item, css_item)
-    elif type == 'combined':
+    elif conversion_type == 'combined':
         process_folder_into_file(input_item, output_item, css_item)
     else:
         process_folder_into_folder(input_item, output_item, css_item)
-
+        
     print(f"Successfully converted '{input_item}' to '{output_item}'.")
